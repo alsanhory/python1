e03c9fbbd02de284034482e893fb35007f018cd5
unknown <elrayah.alsanhory@gmail.com>
2018-12-02 19:58:30 +0200

    Changing file

--- testhuda.py
+++
@@ -1,7 +0,0 @@
-def checkname(name):
-   if name.lower()=="huda":
-      print("Huda is my love")
-   else:
-      print("You are not huda")
-
-checkname("HUDA")
---
+++ testname.py
@@ -0,0 +1,8 @@
+def checkname(name):
+   if name.lower()=="nafisa":
+      print("How are you Nafisa")
+   else:
+      print("You are not Nafisa")
+
+name= input("Enter the name")
+checkname(name)

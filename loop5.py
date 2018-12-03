af208423908b3c1beee876e5b6129c1dcfb71086
unknown <elrayah.alsanhory@gmail.com>
2018-12-03 18:52:12 +0200

    Loop questions in loop 5

--- loop5.py
+++ loop5.py
@@ -10,3 +10,17 @@ def seeprimes(num):
     print(newlist)
 
 seeprimes(100)
+
+Print nafisa 10 times
+
+Print numbers from 0 to 10
+
+""" 
+Write a function to print event numbers from given range
+Write a function to calculate sum of given range of numbers
+Write a function to remove duplications from  a list
+Write a function to find factorial of given number
+Write a function to all prime nubmers of given list of numbers
+
+
+ """
